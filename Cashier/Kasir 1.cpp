60b13bd8103a8f5a0ddb36ae39ecaad560986028
Virya Dharmawan <viryadharmawan@gmail.com>
2016-12-15 15:53:32 +0700

    Persistence Update
    Added save/load functionality, with the program requiring data.txt to be present and properly formatted before the program starts
    If data.txt didn't exist, it will be created with default values
    
    A loop is introduced in the Product Code selection

--- Cashier/Kasir 1.cpp
+++ Cashier/Kasir 1.cpp
@@ -1,20 +1,59 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 #include"fitos.h"
 
+const int EXP_VAL = 3; //Expected no. of menu to read
+char itemnames[3][40] = {};
+int price[3]={}, stock[3] ={};
+
+int load()
+{
+	int i;
+	FILE * f = fopen("data.txt","r");
+	if(f == NULL)
+	{
+		f = fopen("data.txt","w+");
+		fprintf(f,"Burger,10,10000\nMilkshake,10,10000\nBurger + Milkshake,10,10000\n\nHint: %s,%s,%s //DO NOT CHANGE THE NO. OF ITEMS! JANGAN MENGUBAH JUMLAH ITEM!","Nama Produk","Stok", "Harga");
+		fclose(f);
+		printf("ERROR: File not found for data.txt. Dummy data have been saved to the disk.\nPlease modify data.txt with the correct data, then run the application again\n\n");
+		getchar();
+		exit(1);
+	}
+	for (i = 0; i<EXP_VAL;i++)
+	{
+		if(fscanf(f,"%[^,],%d,%d\n",itemnames[i],&stock[i],&price[i]) == EOF) break;
+	}
+	fclose(f);
+	return i;
+}
+
+void save()
+{
+	FILE * f = fopen("data.txt","w");
+	
+	for(int i = 0; i<EXP_VAL;i++)
+	{
+		fprintf(f,"%s,%d,%d\n",itemnames[i],stock[i],price[i]);
+	}
+	fprintf(f,"\nHint: %s,%s,%s //DO NOT CHANGE THE NO. OF ITEMS! JANGAN MENGUBAH JUMLAH ITEM!\n","Nama Produk","Stok", "Harga");
+	fclose(f);
+}
+
 int main(){
+
 	//Kode "burger" & 001 = Burger
 	//Kode "milk" & 002 = Milkshake
 	//Kode "b+m" & 003 = Burger + Milk
 	//Bisa pakai string, angka, atau kombinasi
 	//DONE
-	char itemnames[3][40] = {"Burger", "Milkshake", "Burger + Milkshake"};
-	int price[3] = {15000, 10000, 22000}, stock[3] = {60, 100, 45};
+	
 	int bought[3] = {};
 	int subtotalbayar[3] = {};
 
 	int total = 0, dibayarbrp, kembalian;
 	char transaksidone;
+	load();
 	do
 	{
 		printf("%-5s|%-25s|%-5s|%-8s|%-8s|%s","Kode","Nama Produk","Stok", "Harga", "Dibeli", "Subtotal");
@@ -25,59 +64,76 @@ int main(){
 			fitos(subtotalbayar[i],sub);
 			printf("\n%03d  |%-25s|%-5d|%-8s|%-8d|%-8s",i+1,itemnames[i],stock[i],fit, bought[i], sub);
 		}
-		char itemcode[10];
-		printf("\nMasukkan Kode Barang: ");
-		scanf("%s", &itemcode); fflush(stdin);
+		
+
 
 		//Habis ngetik kode, minta jumlah yang dibeli, dgn jumlah tdk lebih besar dr stok; dan kalau ga ngisi (dienter), dianggap 1
 		//Kondisi while buat variabel baru, dan hasil itemcodenya pakai if dgn do-while di dalamnya if
 		//DONE
-	
-		bool comp;
-		int tempbought;
-		if (strcmp(itemcode, "001") == 0){
-			do{
-				printf("Masukkan jumlah %s yang ingin dibeli [1-%d]: ",itemnames[0],stock[0]);
-				scanf("%d", &tempbought); fflush(stdin);
-				if (tempbought<1||tempbought>stock[0]) printf("Jumlah %s yang dimasukkan salah. Jumlahnya harus diantara 1 dan %d\n", itemnames[0], stock[0]);
-			} while (tempbought<1||tempbought>stock[0]);
-			bought[0] += tempbought;
-			stock[0] -= bought[0];
-			subtotalbayar[0] = bought[0] * price[0];
-		}
-		else if (strcmp(itemcode, "002") == 0){
-			do{
-				printf("Masukkan jumlah %s yang ingin dibeli [1-%d]: ",itemnames[1],stock[1]);
-				scanf("%d", &tempbought); fflush(stdin);
-				if (tempbought<1||tempbought>stock[1]) printf("Jumlah %s yang dimasukkan salah. Jumlahnya harus diantara 1 dan %d\n", itemnames[1], stock[1]);
-			} while (tempbought<1||tempbought>stock[1]);
-			bought[1] += tempbought;
-			stock[1] -= bought[1];
-			subtotalbayar[1] = bought[1] * price[1];
-		}
-		else if (strcmp(itemcode, "003") == 0){
-			do{
-				printf("Masukkan jumlah %s yang ingin dibeli [1-%d]: ",itemnames[2],stock[2]);
-				scanf("%d", &tempbought); fflush(stdin);
-				if (tempbought<1||tempbought>stock[2]) printf("Jumlah %s yang dimasukkan salah. Jumlahnya harus diantara 1 dan %d\n", itemnames[2], stock[2]);
-			} while (tempbought<1||tempbought>stock[2]);
-			bought[2] += tempbought;
-			stock[2] -= bought[2];
-			subtotalbayar[2] = bought[2] * price[2];
-		}
-		else printf("Kode yang diminta tidak tersedia\n");
+		do
+		{		
+			char itemcode[10];
+			printf("\nMasukkan Kode Barang: ");
+			scanf("%s", &itemcode); fflush(stdin);
+			int tempbought;
+			if (strcmp(itemcode, "001") == 0)
+			{
+				do
+				{
+					printf("Masukkan jumlah %s yang ingin dibeli [1-%d]: ",itemnames[0],stock[0]);
+					scanf("%d", &tempbought); fflush(stdin);
+					if (tempbought<1||tempbought>stock[0]) printf("Jumlah %s yang dimasukkan salah. Jumlahnya harus diantara 1 dan %d\n", itemnames[0], stock[0]);
+				} 
+				while (tempbought<1||tempbought>stock[0]);
+				bought[0] += tempbought;
+				stock[0] -= bought[0];
+				subtotalbayar[0] = bought[0] * price[0];
+				break;
+			}
+			else if (strcmp(itemcode, "002") == 0)
+			{
+				do
+				{
+					printf("Masukkan jumlah %s yang ingin dibeli [1-%d]: ",itemnames[1],stock[1]);
+					scanf("%d", &tempbought); fflush(stdin);
+					if (tempbought<1||tempbought>stock[1]) printf("Jumlah %s yang dimasukkan salah. Jumlahnya harus diantara 1 dan %d\n", itemnames[1], stock[1]);
+				} 
+				while (tempbought<1||tempbought>stock[1]);
+				bought[1] += tempbought;
+				stock[1] -= bought[1];
+				subtotalbayar[1] = bought[1] * price[1];
+				break;
+			}
+			else if (strcmp(itemcode, "003") == 0)
+			{
+				do
+				{
+					printf("Masukkan jumlah %s yang ingin dibeli [1-%d]: ",itemnames[2],stock[2]);
+					scanf("%d", &tempbought); fflush(stdin);
+					if (tempbought<1||tempbought>stock[2]) printf("Jumlah %s yang dimasukkan salah. Jumlahnya harus diantara 1 dan %d\n", itemnames[2], stock[2]);
+				} 
+				while (tempbought<1||tempbought>stock[2]);
+				bought[2] += tempbought;
+				stock[2] -= bought[2];
+				subtotalbayar[2] = bought[2] * price[2];
+				break;
+			}
+			else printf("Kode yang diminta tidak tersedia\n");
+		} 
+		while(true);
+
+
 		//Hitung kembali stok berdasarkan total yang dibeli
 
 		//Ketik apa gitu buat konfirmasi transaksi selesai, apanya terserah aku
 		//Stlh selesai, nunjukin total sama ttn, diskon, dsbnya
 		
-		printf("Apakah transaksi sudah selesai? (Y/N");
-		scanf("%s", &transaksidone); fflush(stdin);
-		if(transaksidone == 'n' || transaksidone == 'N') break;
+		printf("Apakah transaksi sudah selesai? (Y/N) ");
+		scanf("%c", &transaksidone); fflush(stdin);
+		if(transaksidone == 'y' || transaksidone == 'Y') break;
 		printf("\n");
 	} 
 	while (true);
-
 	//Hitung total
 	
 	printf("\n\n%-25s|%-8s|%s","Nama Produk", "Dibeli", "Subtotal");
@@ -101,7 +157,7 @@ int main(){
 	kembalian = dibayarbrp-total;
 	fitos(kembalian,kmb);
 	printf("\n\nKembalian: %s\nTerima kasih", kmb);
-		//Stlh ditunjukin total, input dibayar brp, trus dikasih tahu kembaliannya, message thx, lalu kalau dienter kembali ke step awal
 	getchar();
+	save();
 	return 0;
 }
